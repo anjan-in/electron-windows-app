4d8078873d5289310525508e381d4d0f34853ce4
aves94 <anjansen30@gmail.com>
2025-02-25 14:11:20 +0530

    Initial commit - Electron Windows App, added setup files and .gitignore

---
+++ src/main.js
@@ -0,0 +1,22 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'), // Preload script
+            contextIsolation: true,
+            nodeIntegration: false,
+        }
+    });
+
+    win.loadFile(path.join(__dirname, 'renderer', 'index.html'));
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+});
